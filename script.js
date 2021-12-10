fc6c81a8befde9d3370d4f6c608ea638ba781155
husaininajib <husaininajib98@gmail.com>
2021-12-10 14:18:19 +0800

    added hamburger animation

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger")
+const navPanel = document.querySelector(".nav-panel")
+
+
+hamburger.addEventListener("click", () => {
+    navPanel.classList.toggle("show")
+    hamburger.classList.toggle("rotate")
+})
