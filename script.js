27e590c1a81b59255c0e4764f207a52e98022e6a
husaininajib <husaininajib98@gmail.com>
2021-12-12 00:26:35 +0800

    added another event listener

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const hamburger = document.querySelector(".hamburger")
 const navPanel = document.querySelector(".nav-panel")
+const nav = document.querySelector("nav")
+const categoryButton = document.querySelector(".category-hamburger")
+const categoryPanel = document.querySelector(".category-panel")
+const arrow = document.querySelector(".arrow")
 
 
 hamburger.addEventListener("click", () => {
     navPanel.classList.toggle("show")
     hamburger.classList.toggle("rotate")
 })
-// window.addEventListener("scroll", () => {
-//     if (window.pageYOffset > 0) {
-//         navPanel.classList.add("")
-//     }
-// })
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.pageYOffset
+    if (scrollHeight === 0) {
+        nav.classList.remove("insert-border")
+        
+    } else {
+        nav.classList.add("insert-border")
+    }
+})
+categoryButton.addEventListener("click", () => {
+    categoryPanel.classList.toggle("show-category-list")
+    arrow.classList.toggle("rotate")
+})
