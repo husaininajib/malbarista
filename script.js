db408b415bf07d18660d34975f4f4971335372e1
husaininajib <husaininajib98@gmail.com>
2021-12-12 20:57:16 +0800

    added js for arrow home btn

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const nav = document.querySelector("nav")
 const categoryButton = document.querySelector(".category-hamburger")
 const categoryPanel = document.querySelector(".category-panel")
 const arrow = document.querySelector(".arrow")
+const arrowHome = document.querySelector(".arrow-to-home")
+const header = document.querySelector("header")
 
 
 hamburger.addEventListener("click", () => {
@@ -12,14 +14,28 @@ hamburger.addEventListener("click", () => {
 })
 window.addEventListener("scroll", () => {
     const scrollHeight = window.pageYOffset
+    const headerOffsetTop = header.offsetTop
+    const headerHeight = header.clientHeight
     if (scrollHeight === 0) {
         nav.classList.remove("insert-border")
+        arrowHome.classList.remove("appear")
         
     } else {
         nav.classList.add("insert-border")
+    }
+    if (scrollHeight > headerHeight) {
+        console.log("yes")
+        arrowHome.classList.add("appear")
     }
 })
 categoryButton.addEventListener("click", () => {
     categoryPanel.classList.toggle("show-category-list")
     arrow.classList.toggle("rotate")
 })
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.pageYOffset
+    // const sectionOffset = 
+})
+
+const section = document.querySelectorAll("section")
+
