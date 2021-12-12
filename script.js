7e0aa741bd2f5a922144b0ca5242b91dc8a723e4
husaininajib <husaininajib98@gmail.com>
2021-12-13 07:41:58 +0800

    added new features in review-popup

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 const hamburger = document.querySelector(".hamburger")
 const navPanel = document.querySelector(".nav-panel")
 const nav = document.querySelector("nav")
-const categoryButton = document.querySelector(".category-hamburger")
-const categoryPanel = document.querySelector(".category-panel")
 const arrow = document.querySelector(".arrow")
 const arrowHome = document.querySelector(".arrow-to-home")
 const header = document.querySelector("header")
-const closeButton = document.querySelectorAll(".close")
-const orderNowButton = document.querySelector(".order-now-button")
-const foodpandaPopup = document.querySelector(".foodpanda-popup")
-const reviewPopup = document.querySelector(".review-popup")
-const reviewButton = document.querySelector(".review-button")
-
 
 hamburger.addEventListener("click", () => {
     navPanel.classList.toggle("show")
@@ -30,19 +22,32 @@ window.addEventListener("scroll", () => {
         nav.classList.add("insert-border")
     }
     if (scrollHeight > headerHeight) {
-        console.log("yes")
         arrowHome.classList.add("appear")
     }
 })
+
+const categoryButton = document.querySelector(".category-hamburger")
+const categoryPanel = document.querySelector(".category-panel")
+
 categoryButton.addEventListener("click", () => {
     categoryPanel.classList.toggle("show-category-list")
     arrow.classList.toggle("rotate")
 })
 
+const closeButton = document.querySelectorAll(".close")
+const orderNowButton = document.querySelectorAll(".order-now-button")
+const foodpandaPopup = document.querySelector(".foodpanda-popup")
+const reviewPopup = document.querySelector(".review-popup")
+const reviewButton = document.querySelector(".review-button")
 
-orderNowButton.addEventListener("click", () => {
-    foodpandaPopup.classList.add("show-popup")
+orderNowButton.forEach(button => {
+    button.addEventListener("click", () => {
+        foodpandaPopup.classList.add("show-popup")
+    })
 })
+// orderNowButton.addEventListener("click", () => {
+//     foodpandaPopup.classList.add("show-popup")
+// })
 reviewButton.addEventListener("click", () => {
     reviewPopup.classList.add("show-review")
 })
@@ -52,4 +57,28 @@ closeButton.forEach(button => {
         reviewPopup.classList.remove("show-review")
     })
 })
+
+const reviewTextarea = document.querySelector("textarea")
+const username = document.querySelector("#username")
+const reviewSubmitButton = document.querySelector(".review-submit-btn")
+const form = document.querySelector("form")
+const successText = document.querySelector(".review-success-container")
+
+form.addEventListener("submit", (ev) => {
+    const customerReview = reviewTextarea.value
+    const usernameValue = username.value
+    ev.preventDefault()
+    if (customerReview === "" || customerReview == null) {
+        console.log("no review")
+    } else {
+        form.reset()
+        successText.classList.add("show-success")
+        console.log(customerReview)
+        localStorage.setItem("Review", customerReview)
+
+    }
+})
+
+
+
 
